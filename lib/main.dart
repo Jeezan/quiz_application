a0b6b5db3254e3c2ab685eb3649bbe50743f1907
N.K Jeezan Ahmed <nkjeezanahamed1@gmail.com>
2025-07-11 20:19:52 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_application/views/start_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: StartScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
