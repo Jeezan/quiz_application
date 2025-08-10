37d1cd7c173554ffbd7859faa386a472dbfa6910
N.K Jeezan Ahmed <nkjeezanahamed1@gmail.com>
2025-08-10 11:06:01 +0530

    Refactor: Move question data to a new data model and update imports

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,67 +1,76 @@
-List<Map<String, dynamic>> questions = [
-  {
-    "question": "What is Flutter?",
-    "options": [
+import 'package:quiz_application/models/questions.dart';
+
+const List<Questions> questions = [
+
+  Questions(
+    questions: "What is Flutter?", 
+    options: [
       "A UI toolkit for building natively compiled applications",
       "A backend framework for web development",
       "A library for building Android apps only",
       "A text editing tool",
       "A mobile development framework"
     ]
-  },
-  {
-    "question": "Which language is used to write Flutter apps?",
-    "options": [
+  ),
+
+  Questions(
+    questions: "Which language is used to write Flutter apps?",
+    options: [
       "Dart",
       "Java",
       "Swift",
       "Kotlin",
       "C++"
     ]
-  },
-  {
-    "question": "Which widget is used to create a button in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which widget is used to create a button in Flutter?",
+    options: [
       "ElevatedButton",
       "FlatButton",
       "TextButton",
       "IconButton",
       "RaisedButton"
     ]
-  },
-  {
-    "question": "What is the main purpose of the `setState()` method in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "What is the main purpose of the `setState()` method in Flutter?",
+    options: [
       "To update the UI by marking the widget as dirty",
       "To create a new widget",
       "To handle user input",
       "To initialize variables",
       "To trigger a state change in the widget"
     ]
-  },
-  {
-    "question": "Which of the following is a stateless widget in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which of the following is a stateless widget in Flutter?",
+    options: [
       "Text",
       "Checkbox",
       "Switch",
       "TextField",
       "Container"
     ]
-  },
-  {
-    "question": "Which function is called first in Flutter when an app starts?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which function is called first in Flutter when an app starts?",
+    options: [
       "main()",
       "build()",
       "runApp()",
       "initState()",
       "start()"
     ]
-  },
-  {
-    "question": "Which widget is used to display an image in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which widget is used to display an image in Flutter?",
+    options: [
       "Image",
       "Icon",
       "AssetImage",
@@ -69,20 +78,22 @@ List<Map<String, dynamic>> questions = [
       "Picture",
       "SvgPicture"
     ]
-  },
-  {
-    "question": "Which of the following is used for routing in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which of the following is used for routing in Flutter?",
+    options: [
       "Navigator",
       "RouteBuilder",
       "ScreenManager",
       "PageController",
       "Router"
     ]
-  },
-  {
-    "question": "What does the `FutureBuilder` widget do in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "What does the `FutureBuilder` widget do in Flutter?",
+    options: [
       "Builds a widget based on the state of a `Future`",
       "Creates a new future for asynchronous tasks",
       "Renders a static widget",
@@ -90,20 +101,22 @@ List<Map<String, dynamic>> questions = [
       "Fetches data from an API",
       "Waits for an asynchronous task to complete before rendering"
     ]
-  },
-  {
-    "question": "Which package is used to manage state in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which package is used to manage state in Flutter?",
+    options: [
       "Provider",
       "SQLite",
       "Http",
       "SharedPreferences",
       "BLoC"
     ]
-  },
-  {
-    "question": "Which widget can be used to create a scrollable list in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which widget can be used to create a scrollable list in Flutter?",
+    options: [
       "ListView",
       "Column",
       "Row",
@@ -111,95 +124,104 @@ List<Map<String, dynamic>> questions = [
       "SingleChildScrollView",
       "GridView"
     ]
-  },
-  {
-    "question": "What is the default layout widget in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "What is the default layout widget in Flutter?",
+    options: [
       "Container",
       "Scaffold",
       "Column",
       "Stack",
       "BoxDecoration"
     ]
-  },
-  {
-    "question": "What is the main purpose of `const` keyword in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "What is the main purpose of `const` keyword in Flutter?",
+    options: [
       "To create compile-time constant widgets",
       "To define static variables",
       "To declare a function",
       "To enable hot reload",
       "To optimize memory"
     ]
-  },
-  {
-    "question": "Which Flutter widget is used to display a loading indicator?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which Flutter widget is used to display a loading indicator?",
+    options: [
       "CircularProgressIndicator",
       "Text",
       "ProgressBar",
       "ActivityIndicator",
       "LinearProgressIndicator"
     ]
-  },
-  {
-    "question": "Which method is used to change the state of a `StatefulWidget`?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which method is used to change the state of a `StatefulWidget`?",
+    options: [
       "setState()",
       "updateState()",
       "build()",
       "changeState()",
       "rebuild()"
     ]
-  },
-  {
-    "question": "What does the `Navigator.push()` method do in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "What does the `Navigator.push()` method do in Flutter?",
+    options: [
       "Pushes a new route onto the navigation stack",
       "Pops the current route from the stack",
       "Replaces the current route",
       "Checks if a route exists in the stack",
       "Shows a dialog"
     ]
-  },
-  {
-    "question": "Which widget is used for making a custom layout in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which widget is used for making a custom layout in Flutter?",
+    options: [
       "CustomPaint",
       "Column",
       "Row",
       "Container",
       "LayoutBuilder"
     ]
-  },
-  {
-    "question": "What is the purpose of `InheritedWidget` in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "What is the purpose of `InheritedWidget` in Flutter?",
+    options: [
       "To share data efficiently across the widget tree",
       "To render a widget with custom data",
       "To make a widget editable",
       "To optimize performance in animations",
       "To manage states across screens"
     ]
-  },
-  {
-    "question": "Which widget in Flutter is used to create a multi-line text input field?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "Which widget in Flutter is used to create a multi-line text input field?",
+    options: [
       "TextField",
       "Text",
       "Form",
       "RichText",
       "MultiTextField"
     ]
-  },
-  {
-    "question": "How do you make a widget scrollable in Flutter?",
-    "options": [
+  ),
+  
+  Questions(
+    questions: "How do you make a widget scrollable in Flutter?",
+    options: [
       "Wrap it in a `SingleChildScrollView`",
       "Use a `ListView`",
       "Use `Scrollbar`",
       "Apply `Overflow` property",
       "Wrap it in a `CustomScrollView`"
     ]
-  }
+  ),
 ];
---
+++ lib/models/questions.dart
@@ -0,0 +1,14 @@
+class Questions{
+
+  final String questions;
+  final List<String> options;
+
+  const Questions({
+    required this.questions,
+    required this.options
+    });
+
+}
+
+// return List<String> randomAnswer
+ 
